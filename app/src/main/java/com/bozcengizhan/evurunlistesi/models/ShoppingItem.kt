567355005717a9008120a8db0882cefea22770c8
Cengizhan Boz <bozcengizhan2@gmail.com>
2026-06-08 15:02:23 +0300

    düzenlemeler.

--- app/src/main/java/com/bozcengizhan/evurunlistesi/models/ShoppingItem.kt
+++ app/src/main/java/com/bozcengizhan/evurunlistesi/models/ShoppingItem.kt
@@ -1,7 +1,7 @@
 package com.bozcengizhan.evurunlistesi.models
 
 data class ShoppingItem(
-    val id: String = "",
-    val name: String = "",
+    val id: String = " ",
+    val name: String = " ",
     val isBought: Boolean = false
 )
